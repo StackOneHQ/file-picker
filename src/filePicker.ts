a9e6fff0c3174beffaab819396283f25d8b17cf6
Jorge Rebocho <jorge@stackone.com>
2024-12-16 18:29:45 +0000

    fix: first release (#1)

--- src/filePicker.ts
+++ src/filePicker.ts
@@ -1,6 +1,6 @@
 // dummy code for testing - DELETE ME
 export const openFilePicker = async () => {
-    console.log('Opening file picker...');
+    console.log('Opening file picker dummy message...');
 
     return true;
 };
