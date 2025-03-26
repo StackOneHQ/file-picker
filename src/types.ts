7a01c1a8bd3879d37b9f3fa466ba81a2c765485a
Joao Miguel <joao@stackone.com>
2025-03-26 15:10:09 +0000

    chore: file picker v1

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface FilePickerOptions {
+    containerId?: string;
+    sessionToken: string;
+    baseUrl?: string;
+    onFilesPick?: (data: unknown) => void;
+    onClose?: () => void;
+    onOpen?: () => void;
+    onCancel?: () => void;
+  }
